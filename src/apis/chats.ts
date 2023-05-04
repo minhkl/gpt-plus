c8eb84bf7e8a4fda23054c02b4a1145a75d69fa6
Luong Kien Minh <luongkienminh.it@gmail.com>
2023-05-04 22:59:11 +0700

    update: api host

--- src/apis/chats.ts
+++ src/apis/chats.ts
@@ -1,12 +1,6 @@
 import customAxios from "@/utils/customAxios";
 import { IChatCompletionsPostParams, IChatCompletionPostResponse } from "@/app/api/chats/route";
 
-export function apiChatsPost({ prompt }: { prompt: string }) {
-  return customAxios.post<{ message: string }>("/api/chats", {
-    prompt,
-  });
-}
-
 export function apiChatCompletionsPost(params: IChatCompletionsPostParams) {
-  return customAxios.post<IChatCompletionPostResponse>("/api/chats/", params);
+  return customAxios.post<IChatCompletionPostResponse>("/chats/", params);
 }
