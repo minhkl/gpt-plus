7a8c3115695cd6e1f87787a25fcbd73727b73866
Luong Kien Minh <luongkienminh.it@gmail.com>
2023-05-25 18:03:51 +0700

    feat: add script "yarn analyze"

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+const nextConfig = withBundleAnalyzer({
   experimental: {
     appDir: true,
   },
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
